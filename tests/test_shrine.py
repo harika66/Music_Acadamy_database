fee6ed89883697dffa71f3f15fc6623e24cf593e
Gopika <higopika@gmail.com>
2021-12-06 11:00:08 +0530

    fixed post request test failure

--- tests/test_shrine.py
+++ tests/test_shrine.py
@@ -1,21 +1,21 @@
 import requests
 import json
 import jsonpath
+import names
 
-# def test_add_student():    
-#     url = "http://shrine_fastapi:80/user"
-# #    user = {"data" : { "std_name": "Pranav1111", "course_name": "guitar", "batch": "string", "tch_name": "string", "fees": 0 }}
+def test_add_student():
+    url = "http://shrine_fastapi:80/user/"
+    headers = {'content-type': 'application/json'}
+    user = { "std_name": "", "course_name": "guitar", "batch": "string", "tch_name": "string", "fees": 0 }
+    user["std_name"] = names.get_full_name()
+    print( "Name : ", user["std_name"] )
 
+    response = requests.post(url,data=json.dumps(user), headers = headers)
+    print(response.request.url)
+    print(response.request.headers)
+    print(response.request.body)
+    assert response.status_code == 200
 
-#     file = open("/test/add_student.json",'r',encoding='utf-8')
-#     json_input = file.read()
-#     request_json = json.loads(json_input)
-#     print(request_json)
-#     response = requests.post(url,request_json)
-#     print(response.request.url)
-#     print(response.request.headers)
-#     print(response.request.body)
-#     assert response.status_code == 200    
 
 
 def test_get_all_students():
